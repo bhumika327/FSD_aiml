889e5035e82a56af14c31bc16cc474d127e858fe
bhumika327 <bhumika.23b1531008@abes.ac.in>
2025-11-10 08:53:50 +0530

    react work

---
+++ javascript/ReactWork/ReactViteApp/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './index.css'
+import Profile from './components/Profile'
+function App() {
+  return (
+    <>
+      <Profile />
+    </>
+  )
+}
+
+export default App
---
+++ javascript/ReactWork/ReactViteApp/src/components/Profile.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './profile.css'
+export default function  Profile() {
+  return (
+    <div className='container'>
+    <h2>welcome</h2>
+    </div>
+  )
+}
